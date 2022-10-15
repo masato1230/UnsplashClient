4a4cf7221f22e75fb6ac4ecbeb6806089cf6baf6
masato1230 <0ht3851f11t426k@ezweb.ne.jp>
2022-10-15 16:31:40 +0900

    Add models

---
+++ app/src/main/java/com/example/unsplashclient/domain/model/PhotoDetail.kt
@@ -0,0 +1,12 @@
+package com.example.unsplashclient.domain.model
+
+data class PhotoDetail(
+    val description: String,
+    val likes: Int,
+    val imageUrl: Int,
+    val photographer: Int,
+    val camera: String,
+    val location: String,
+    val views: Int,
+    val downloads: Int,
+)
