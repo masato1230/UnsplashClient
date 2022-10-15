db1ed4d5b39a289534625a795f419f5e8c78731d
masato1230 <0ht3851f11t426k@ezweb.ne.jp>
2022-10-15 13:55:22 +0900

    Add photodetail retrofit interface

--- app/src/main/java/com/example/unsplashclient/data/remote/UnsplashApi.kt
+++ app/src/main/java/com/example/unsplashclient/data/remote/UnsplashApi.kt
@@ -1,9 +1,11 @@
 package com.example.unsplashclient.data.remote
 
 import com.example.unsplashclient.common.Constants
+import com.example.unsplashclient.data.remote.dto.PhotoDetailDto
 import com.example.unsplashclient.data.remote.dto.SearchPhotosResultDto
 import retrofit2.http.GET
 import retrofit2.http.Headers
+import retrofit2.http.Path
 import retrofit2.http.Query
 
 interface UnsplashApi {
@@ -11,4 +13,8 @@ interface UnsplashApi {
     @Headers("Authorization: Client-ID ${Constants.API_KEY}")
     @GET("search/photos/{query}")
     suspend fun searchPhotos(@Query("query") query: String): SearchPhotosResultDto
+
+    @Headers("Authorization: Client-ID ${Constants.API_KEY}")
+    @GET("photos/{id}")
+    suspend fun getPhotoById(@Path("id") photoId: String): PhotoDetailDto
 }
