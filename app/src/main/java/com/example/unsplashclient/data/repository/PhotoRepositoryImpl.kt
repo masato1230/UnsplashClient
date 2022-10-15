1a810c5b398d1022661caa54af9637fc01b61134
masato1230 <0ht3851f11t426k@ezweb.ne.jp>
2022-10-15 14:01:19 +0900

    Add get photo detail function to repository

--- app/src/main/java/com/example/unsplashclient/data/repository/PhotoRepositoryImpl.kt
+++ app/src/main/java/com/example/unsplashclient/data/repository/PhotoRepositoryImpl.kt
@@ -1,6 +1,7 @@
 package com.example.unsplashclient.data.repository
 
 import com.example.unsplashclient.data.remote.UnsplashApi
+import com.example.unsplashclient.data.remote.dto.PhotoDetailDto
 import com.example.unsplashclient.data.remote.dto.SearchPhotosResultDto
 import com.example.unsplashclient.domain.repository.PhotoRepository
 import javax.inject.Inject
@@ -11,5 +12,9 @@ class PhotoRepositoryImpl @Inject constructor(
 
     override suspend fun searchPhotos(query: String): SearchPhotosResultDto {
         return api.searchPhotos(query)
+    }
+
+    override suspend fun getPhotoById(photoId: String): PhotoDetailDto {
+        return api.getPhotoById(photoId)
     }
 }
--- app/src/main/java/com/example/unsplashclient/domain/repository/PhotoRepository.kt
+++ app/src/main/java/com/example/unsplashclient/domain/repository/PhotoRepository.kt
@@ -1,8 +1,11 @@
 package com.example.unsplashclient.domain.repository
 
+import com.example.unsplashclient.data.remote.dto.PhotoDetailDto
 import com.example.unsplashclient.data.remote.dto.SearchPhotosResultDto
 
 interface PhotoRepository {
 
     suspend fun searchPhotos(query: String): SearchPhotosResultDto
+
+    suspend fun getPhotoById(photoId: String): PhotoDetailDto
 }
