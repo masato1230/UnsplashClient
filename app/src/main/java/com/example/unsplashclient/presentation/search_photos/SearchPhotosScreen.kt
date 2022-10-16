2dbf7e13539e030bfe8f21e5740b8a6f02ad3362
masato1230 <0ht3851f11t426k@ezweb.ne.jp>
2022-10-16 13:35:24 +0900

    Create list ui

--- app/src/main/java/com/example/unsplashclient/presentation/search_photos/SearchPhotosScreen.kt
+++ app/src/main/java/com/example/unsplashclient/presentation/search_photos/SearchPhotosScreen.kt
@@ -2,9 +2,9 @@ package com.example.unsplashclient.presentation.search_photos
 
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.hilt.navigation.compose.hiltViewModel
+import com.example.unsplashclient.presentation.search_photos.components.PhotoThumbnail
 
 @Composable
 fun SearchPhotosScreen(
@@ -13,7 +13,9 @@ fun SearchPhotosScreen(
     val state = viewModel.state.value
     LazyColumn {
         items(state.photos) { photo ->
-            Text(text = photo.imageUrl)
+            PhotoThumbnail(photo = photo) {
+                // TODO
+            }
         }
     }
 }
