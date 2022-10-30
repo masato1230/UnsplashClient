ae9f3005db86835ac00e9225352ea5f37b0fb7a9
masato1230 <0ht3851f11t426k@ezweb.ne.jp>
2022-10-30 19:11:08 +0900

    12_add_get_photo_by_id_to_repository

--- app/src/main/java/com/example/unsplashclient/data/repository/PhotoRepositoryImpl.kt
+++ app/src/main/java/com/example/unsplashclient/data/repository/PhotoRepositoryImpl.kt
@@ -1,5 +1,6 @@
 package com.example.unsplashclient.data.repository
 
+import com.example.unsplashclient.data.remote.PhotoDetailDto
 import com.example.unsplashclient.data.remote.SearchPhotosResultDto
 import com.example.unsplashclient.data.remote.UnsplashApi
 import com.example.unsplashclient.domain.repository.PhotoRepository
@@ -11,5 +12,9 @@ class PhotoRepositoryImpl @Inject constructor(
 
     override suspend fun searchPhotos(query: String): SearchPhotosResultDto {
         return api.searchPhotos(query)
+    }
+
+    override suspend fun getPhotoById(photoId: String): PhotoDetailDto {
+        return api.getPhotoById(photoId)
     }
 }
