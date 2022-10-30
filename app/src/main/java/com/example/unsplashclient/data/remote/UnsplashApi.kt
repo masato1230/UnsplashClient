c4724ad8d039ddc5c32dba569d682105998a57cd
masato1230 <0ht3851f11t426k@ezweb.ne.jp>
2022-10-30 17:51:39 +0900

    11_add_get_photo_by_id_retrofit_interface

--- app/src/main/java/com/example/unsplashclient/data/remote/UnsplashApi.kt
+++ app/src/main/java/com/example/unsplashclient/data/remote/UnsplashApi.kt
@@ -3,6 +3,7 @@ package com.example.unsplashclient.data.remote
 import com.example.unsplashclient.common.Constants
 import retrofit2.http.GET
 import retrofit2.http.Headers
+import retrofit2.http.Path
 import retrofit2.http.Query
 
 interface UnsplashApi {
@@ -10,4 +11,8 @@ interface UnsplashApi {
     @Headers("Authorization: Client-ID ${Constants.API_KEY}")
     @GET("search/photos")
     suspend fun searchPhotos(@Query("query") query: String): SearchPhotosResultDto
+
+    @Headers("Authorization: Client-ID ${Constants.API_KEY}")
+    @GET("photos/{id}")
+    suspend fun getPhotoById(@Path("id") photoId: String): PhotoDetailDto
 }
