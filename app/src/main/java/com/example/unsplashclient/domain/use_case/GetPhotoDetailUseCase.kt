efee1adb50017add9bf67880f0e6387adc1be922
masato1230 <0ht3851f11t426k@ezweb.ne.jp>
2022-11-05 12:36:20 +0900

    17_get_photo_detail_usecase

---
+++ app/src/main/java/com/example/unsplashclient/domain/use_case/GetPhotoDetailUseCase.kt
@@ -0,0 +1,23 @@
+package com.example.unsplashclient.domain.use_case
+
+import com.example.unsplashclient.common.NetworkResponse
+import com.example.unsplashclient.data.remote.toPhotoDetail
+import com.example.unsplashclient.domain.model.PhotoDetail
+import com.example.unsplashclient.domain.repository.PhotoRepository
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import javax.inject.Inject
+
+class GetPhotoDetailUseCase @Inject constructor(
+    private val repository: PhotoRepository,
+) {
+    operator fun invoke(photoId: String): Flow<NetworkResponse<PhotoDetail>> = flow {
+        try {
+            emit(NetworkResponse.Loading<PhotoDetail>())
+            val photoDetail = repository.getPhotoById(photoId).toPhotoDetail()
+            emit(NetworkResponse.Success<PhotoDetail>(photoDetail))
+        } catch(e: Exception) {
+            emit(NetworkResponse.Failure<PhotoDetail>(e.message.toString()))
+        }
+    }
+}
