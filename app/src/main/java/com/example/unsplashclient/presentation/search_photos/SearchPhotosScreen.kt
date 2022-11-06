c4ced50008257099f2a71175610bf6652a581435
masato1230 <0ht3851f11t426k@ezweb.ne.jp>
2022-11-06 14:51:03 +0900

    28_connect_searchbar_and_viewmodel

--- app/src/main/java/com/example/unsplashclient/presentation/search_photos/SearchPhotosScreen.kt
+++ app/src/main/java/com/example/unsplashclient/presentation/search_photos/SearchPhotosScreen.kt
@@ -19,9 +19,9 @@ fun SearchPhotosScreen(
     Scaffold(
         topBar = {
             SearchBar(
-                searchText = "",
-                onSearchTextChanged = {},
-                onDone = {},
+                searchText = viewModel.query,
+                onSearchTextChanged = { viewModel.query = it },
+                onDone = { viewModel.searchPhotos() },
             )
         }
     ) { paddingValue ->
