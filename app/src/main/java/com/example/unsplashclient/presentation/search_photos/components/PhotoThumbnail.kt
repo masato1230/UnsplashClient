3c05d9cfd0d32cea77be26899bd90fab44ab578a
masato1230 <0ht3851f11t426k@ezweb.ne.jp>
2022-11-06 12:51:55 +0900

    26_add_photo_thumbnail

---
+++ app/src/main/java/com/example/unsplashclient/presentation/search_photos/components/PhotoThumbnail.kt
@@ -0,0 +1,89 @@
+package com.example.unsplashclient.presentation.search_photos.components
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.CircularProgressIndicator
+import androidx.compose.material.Icon
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
+import com.example.unsplashclient.domain.model.Photo
+import com.example.unsplashclient.presentation.ui.theme.UnsplashClientTheme
+
+@Composable
+fun PhotoThumbnail(
+    photo: Photo,
+    onClick: (Photo) -> Unit,
+) {
+    Box(
+        modifier = Modifier
+            .background(Color.Black)
+            .heightIn(min = 200.dp)
+            .clickable { onClick(photo) },
+        contentAlignment = Alignment.BottomCenter,
+    ) {
+        CircularProgressIndicator(modifier = Modifier.align(Alignment.Center))
+        AsyncImage(
+            model = photo.imageUrl,
+            contentDescription = photo.description,
+            modifier = Modifier.fillMaxWidth(),
+        )
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .background(Color.Black.copy(alpha = 0.5f))
+                .padding(10.dp),
+        ) {
+            Column(
+                modifier = Modifier.fillMaxWidth(0.8f),
+            ) {
+                Text(
+                    text = photo.description ?: "No description",
+                    color = Color.White,
+                    style = MaterialTheme.typography.h6,
+                )
+                Text(
+                    text = photo.photographer ?: "Unknown photographer",
+                    color = Color.White,
+                    style = MaterialTheme.typography.body2,
+                )
+            }
+            Spacer(modifier = Modifier.weight(1f))
+            Icon(
+                imageVector = Icons.Default.Favorite,
+                contentDescription = "likes",
+                tint = Color.Magenta,
+            )
+            Spacer(modifier = Modifier.width(5.dp))
+            Text(
+                text = photo.likes.toString(),
+                color = Color.White,
+                style = MaterialTheme.typography.body1,
+            )
+        }
+    }
+}
+
+@Preview
+@Composable
+private fun PhotoThumbnailPreivew() {
+    val photo = Photo(
+        photoId = "",
+        description = "Image description",
+        likes = 100,
+        imageUrl = "https://images.unsplash.com/photo-1665686377065-08ba896d16fd?ixlib=rb-4.0.3&ixid=MnwxMjA3fDF8MHxlZGl0b3JpYWwtZmVlZHwxfHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=800&q=60",
+        photographer = "Surface",
+    )
+    UnsplashClientTheme {
+        PhotoThumbnail(photo = photo, onClick = {})
+    }
+}
