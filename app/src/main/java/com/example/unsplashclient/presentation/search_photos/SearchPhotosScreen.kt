19e0dde13b1536a7a1fceec8a27bfdecac181f51
masato1230 <0ht3851f11t426k@ezweb.ne.jp>
2022-11-13 17:36:32 +0900

    29_add_loading_and_error_display

--- app/src/main/java/com/example/unsplashclient/presentation/search_photos/SearchPhotosScreen.kt
+++ app/src/main/java/com/example/unsplashclient/presentation/search_photos/SearchPhotosScreen.kt
@@ -1,10 +1,16 @@
 package com.example.unsplashclient.presentation.search_photos
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.CircularProgressIndicator
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Scaffold
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.example.unsplashclient.presentation.search_photos.components.PhotoThumbnail
@@ -25,9 +31,25 @@ fun SearchPhotosScreen(
             )
         }
     ) { paddingValue ->
-        LazyColumn(modifier = Modifier.padding(paddingValue)) {
-            items(state.photos) { photo ->
-                PhotoThumbnail(photo = photo, onClick = {})
+        Box(modifier = Modifier.fillMaxSize()) {
+            when {
+                state.isLoading -> {
+                    CircularProgressIndicator(modifier = Modifier.align(Alignment.Center))
+                }
+                !state.error.isNullOrBlank() -> {
+                    Text(
+                        text = state.error,
+                        modifier = Modifier.align(Alignment.Center),
+                        color = MaterialTheme.colors.error,
+                    )
+                }
+                else -> {
+                    LazyColumn(modifier = Modifier.padding(paddingValue)) {
+                        items(state.photos) { photo ->
+                            PhotoThumbnail(photo = photo, onClick = {})
+                        }
+                    }
+                }
             }
         }
     }
